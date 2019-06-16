37b26cfeabc919ffa9ad5f3add73a1cc3a7079c4
Joseph Acosta <ja0310@production.int>
2019-06-15 22:21:45 -0700

    initial

---
+++ sFunctions.psm1
@@ -0,0 +1,188 @@
+<#
+.SYNOPSIS
+ Connects to the SRM instance of the currently connected VCenter
+.DESCRIPTION
+ Connects to the SRM instance of the currently connected VCenter and its paired partner	with the current session
+ username.  Prompts for a SRM password.  Password is applied locally and remotely.
+.EXAMPLE
+ csrm
+#>
+function csrm
+{
+	$CUser=$env:USERDOMAIN;$CUser=$CUser+"\";$CUser=$CUser+$env:USERNAME
+	$CPass=Read-Host -AsSecureString -Prompt "Enter SRM password"
+    #Connect-SrmServer -SrmServerAddress $global:DefaultVIServer -User $CUser -Password $CPass -RemoteUser $CUser -RemotePassword $CPass
+    Connect-SrmServer -SrmServerAddress $DefaultVIServer -User $CUser -Password $CPass -RemoteUser $CUser -RemotePassword $CPass
+}
+
+<#
+.SYNOPSIS
+Sends a Dismiss command to a SRM Recovery Plan
+.DESCRIPTION
+Sends a Dismiss command to a SRM Recovery Plan Prompt to continue plan execution.
+Does not attempt if submitted plan is not in a Prompting state.
+.PARAMETER RecoveryPlan
+SRM Recovery Plan.  VMware.VimAutomation.Srm.Views.SrmRecoveryPlan
+.EXAMPLE
+$p = Get-SRMRecoveryPlan -Name XYZ
+$p | Send-SRMDismiss
+#>
+Function Send-SRMDismiss
+{
+    [cmdletbinding(SupportsShouldProcess = $True , ConfirmImpact = "High")]
+    Param
+    (
+        [Parameter (Mandatory = $true , ValueFromPipeline = $true)]
+        [VMware.VimAutomation.Srm.Views.SrmRecoveryPlan[]] $RecoveryPlan
+    )
+
+    Begin
+    {
+        $ReqState = "Prompting"
+    }
+    Process
+    {
+        foreach ($rp in $RecoveryPlan)
+        {
+            $rpinfo = $rp.GetInfo()
+
+            if ($pscmdlet.ShouldProcess( $rpinfo.Name , "Dismiss Prompt" ))
+            {
+                if ($rpinfo.State -eq $ReqState)
+                {
+                    $rp.AnswerPrompt($rp.ListPrompts().key , $false, "Dismiss")
+                }
+
+                else
+                {
+                    $mesg = "Not Sending Dismissal for $($rpinfo.Name).  State is $($rpinfo.State).  State should be $ReqState."
+                    Write-Output "`n`t`t$mesg`n"
+                }
+            }
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+Starts the SRM Cleanup Process.
+.DESCRIPTION
+Starts the SRM Cleanup Process for specified SRM Recovery Plans.
+Does not attempt if submitted plan is not in a NeedsCleanup state.
+.PARAMETER RecoveryPlan
+SRM Recovery Plan.  VMware.VimAutomation.Srm.Views.SrmRecoveryPlan
+.EXAMPLE
+$p = Get-SRMRecoveryPlan -Name XYZ
+$p | Start-SRMCleanUp
+#>
+Function Start-SRMCleanUp
+{
+    [cmdletbinding(SupportsShouldProcess = $True , ConfirmImpact = "High")]
+    Param
+    (
+        [Parameter (Mandatory = $true , ValueFromPipeline = $true )]
+        [VMware.VimAutomation.Srm.Views.SrmRecoveryPlan[]] $RecoveryPlan
+    )
+
+    Begin
+    {
+        [VMware.VimAutomation.Srm.Views.SrmRecoveryPlanRecoveryMode] $RecoveryMode = [VMware.VimAutomation.Srm.Views.SrmRecoveryPlanRecoveryMode]::CleanUpTest
+        $ReqState = "NeedsCleanup"
+    }
+
+    Process
+    {
+        foreach ($rp in $RecoveryPlan)
+        {
+            $rpinfo = $rp.GetInfo()
+
+            if ($pscmdlet.ShouldProcess($rpinfo.Name, $RecoveryMode))
+            {
+                if ($rpinfo.State -eq $ReqState)
+                {
+                    $rp.Start($RecoveryMode)
+                }
+
+                else
+                {
+                    $mesg = "Not Starting Cleanup for $($rpinfo.Name).  State is $($rpinfo.State).  State should be $ReqState."
+                    Write-Output "`n`t`t$mesg`n"
+                }
+            }
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+Starts a Test SRM Recovery Plan.
+.DESCRIPTION
+Starts a Test SRM Recovery Plan, optionally synching data.
+Does not attempt if submitted plan is not in a Ready state.
+.PARAMETER RecoveryPlan
+SRM Recovery Plan.  VMware.VimAutomation.Srm.Views.SrmRecoveryPlan
+.PARAMETER SyncData
+Future:  Defaults to False.  Can be set True to Sync Data.  Believe exposed in SRM 6.5 API
+.EXAMPLE
+$p = Get-SRMRecoveryPlan -Name XYZ
+$p | Start-SRMTest
+.EXAMPLE
+Future Functionality Below:
+$p = Get-SRMRecoveryPlan -Name XYZ
+$p | Start-SRMTest -SyncData=$False
+#>
+
+Function Start-SRMTest
+{
+    [cmdletbinding(SupportsShouldProcess = $True , ConfirmImpact = "High")]
+    Param
+    (
+        [Parameter (Mandatory = $true, ValueFromPipeline = $true, Position = 1)]
+        [VMware.VimAutomation.Srm.Views.SrmRecoveryPlan[]] $RecoveryPlan,
+
+        [bool] $SyncData = $False
+    )
+
+    Begin
+    {
+        [VMware.VimAutomation.Srm.Views.SrmRecoveryPlanRecoveryMode] $RecoveryMode = [VMware.VimAutomation.Srm.Views.SrmRecoveryPlanRecoveryMode]::Test
+        $ReqState = "Ready"
+
+        <#
+        Below two lines for creating the option to synch or not.  Believe exposed in SRM 6.5 API
+        Also modify the entry in the process block.
+        $rpOpt = New-Object VMware.VimAutomation.Srm.Views.SrmRecoveryOptions
+        $rpOpt.SyncData = $False
+        #>
+
+    }
+
+    Process
+    {
+        foreach ($rp in $RecoveryPlan)
+        {
+            $rpinfo = $rp.GetInfo()
+
+            if ($pscmdlet.ShouldProcess($rpinfo.Name, $RecoveryMode))
+            {
+                if ($rpinfo.State -eq $ReqState)
+                {
+                    <#
+                    With Synch false purposely set, API 6.5 or Higher hopefully.
+                    $rp.Start($RecoveryMode,$rpOpt)
+                    #>
+
+                    $rp.Start($RecoveryMode)
+                }
+
+                else
+                {
+                    $mesg = "Not Starting Test of $($rpinfo.Name).  State is $($rpinfo.State).  State should be $ReqState."
+                    Write-Output "`n`t`t$mesg`n"
+                }
+            }
+        }
+    }
+}
+
+Export-ModuleMember -Function * -Alias * -Cmdlet * -Variable *
