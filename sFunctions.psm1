642da13529a028b2c022ed7c6c63fe92f20dc54a
Joseph Acosta <ja0310@production.int>
2019-06-16 11:29:31 -0700

    added AF

--- sFunctions.psm1
+++ sFunctions.psm1
@@ -223,4 +223,53 @@ Function Start-SRMTest
     }
 }
 
+<#
+.SYNOPSIS
+Stops / cancels an SRM Test.
+.DESCRIPTION
+Stops / cancels an SRM Test for specified SRM Recovery Plans.
+Does not attempt if submitted plan is not in a Running or Prompting state.
+.PARAMETER RecoveryPlan
+SRM Recovery Plan.  VMware.VimAutomation.Srm.Views.SrmRecoveryPlan
+.EXAMPLE
+$p = Get-SRMRecoveryPlan | Where-Object -Property Name -eq "PlanXYZ"
+$p | Stop-SRMTest
+#>
+Function Stop-SRMTest
+{
+    [cmdletbinding(SupportsShouldProcess = $True , ConfirmImpact = "High")]
+    Param
+    (
+        [Parameter (Mandatory = $true , ValueFromPipeline = $true )]
+        [VMware.VimAutomation.Srm.Views.SrmRecoveryPlan[]] $RecoveryPlan
+    )
+
+    Begin
+    {
+        $ReqState = "Running" , "Prompting"
+    }
+
+    Process
+    {
+        foreach ($rp in $RecoveryPlan)
+        {
+            $rpinfo = $rp.GetInfo()
+
+            if ($pscmdlet.ShouldProcess($rpinfo.Name, 'Cancel'))
+            {
+                if ($rpinfo.State -in $ReqState)
+                {
+                    $rp.Cancel()
+                }
+
+                else
+                {
+                    $mesg = "Not Stopping $($rpinfo.Name).  State is $($rpinfo.State).  State should be $($ReqState  -join " or ")."
+                    Write-Output "`n`t`t$mesg`n"
+                }
+            }
+        }
+    }
+}
+
 Export-ModuleMember -Function * -Alias * -Cmdlet * -Variable *
