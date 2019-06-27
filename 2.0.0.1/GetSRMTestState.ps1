541de56a61b53e455519742d5864383a0b613757
Joseph Acosta <ja0310@production.int>
2019-06-26 17:15:51 -0700

    first round

---
+++ 2.0.0.1/GetSRMTestState.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+Returns current state of SRM Test.
+.DESCRIPTION
+Returns current state of SRM Test.  CAN BE RUN WHERE???  Must be run on the recovery site.
+.PARAMETER RecoveryPlan
+SRM Recovery Plan.  VMware.VimAutomation.Srm.Views.SrmRecoveryPlan
+.INPUTS
+VMware.VimAutomation.Srm.Views.SrmRecoveryPlan
+.OUTPUTS
+[pscustomobject] SupSkiFun.SRM.Test.Status
+.EXAMPLE
+$p = Get-SRMRecoveryPlan | Where-Object -Property Name -eq "PlanXYZ"
+$p | Get-SRMTestState
+#>
+Function Get-SRMTestState
+{
+    [cmdletbinding()]
+    Param
+    (
+        [Parameter (Mandatory = $true , ValueFromPipeline = $true )]
+        [VMware.VimAutomation.Srm.Views.SrmRecoveryPlan[]] $RecoveryPlan
+    )
+
+    Process
+    {
+        foreach ($rp in $RecoveryPlan)
+        {
+            $st = $rp.GetInfo().State
+            $lo = [pscustomobject]@{
+                Name = $rp.Name
+                State = $st
+            }
+            $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.SRM.Test.Status')
+            $lo
+        }
+    }
+}
---
+++ 2.0.0.1/StartTestNew.ps1
@@ -0,0 +1,62 @@
+<#
+.SYNOPSIS
+Starts a Test SRM Recovery Plan.
+.DESCRIPTION
+Starts a Test SRM Recovery Plan, optionally synching data.
+Does not attempt if submitted plan is not in a Ready state.  Must be run on the recovery site.
+.PARAMETER RecoveryPlan
+SRM Recovery Plan.  VMware.VimAutomation.Srm.Views.SrmRecoveryPlan
+.PARAMETER SyncData
+Defaults to False.  Can be set True to Sync Data.
+.INPUTS
+VMware.VimAutomation.Srm.Views.SrmRecoveryPlan
+.EXAMPLE
+$p = Get-SRMRecoveryPlan | Where-Object -Property Name -eq "PlanXYZ"
+$p | Start-SRMTest
+.EXAMPLE
+Future Functionality Below:
+$p = Get-SRMRecoveryPlan | Where-Object -Property Name -eq "PlanXYZ"
+$p | Start-SRMTest -SyncData=$False
+#>
+
+Function Start-SRMTestNEW
+{
+    [cmdletbinding(SupportsShouldProcess = $True , ConfirmImpact = "High")]
+    Param
+    (
+        [Parameter (Mandatory = $true, ValueFromPipeline = $true, Position = 1)]
+        [VMware.VimAutomation.Srm.Views.SrmRecoveryPlan[]] $RecoveryPlan,
+
+        [bool] $SyncData = $False
+    )
+
+    Begin
+    {
+        [VMware.VimAutomation.Srm.Views.SrmRecoveryPlanRecoveryMode] $RecoveryMode = [VMware.VimAutomation.Srm.Views.SrmRecoveryPlanRecoveryMode]::Test
+        $ReqState = "Ready"
+        $rpOpt = New-Object VMware.VimAutomation.Srm.Views.SrmRecoveryOptions
+        $rpOpt.SyncData = $SyncData
+    }
+
+    Process
+    {
+        foreach ($rp in $RecoveryPlan)
+        {
+            $rpinfo = $rp.GetInfo()
+
+            if ($pscmdlet.ShouldProcess($rpinfo.Name, $RecoveryMode))
+            {
+                if ($rpinfo.State -eq $ReqState)
+                {
+                    $rp.Start($RecoveryMode,$rpOpt)
+                }
+
+                else
+                {
+                    $mesg = "Not Starting Test of $($rpinfo.Name).  State is $($rpinfo.State).  State should be $ReqState."
+                    Write-Output "`n`t`t$mesg`n"
+                }
+            }
+        }
+    }
+}
