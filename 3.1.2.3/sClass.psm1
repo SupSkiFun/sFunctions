60db03509289418b83a9b706e3c66f4a820573cb
Joseph Acosta <ja0310@production.int>
2020-04-20 10:43:14 -0700

    Ready Final Test

--- 3.1.2.3/sClass.psm1
+++ 3.1.2.3/sClass.psm1
@@ -75,4 +75,86 @@ class sClass
         $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.SRM.VM.Info')
         return $lo
     }
+
+    static [pscustomobject] MakePGInfoObj ( [psobject] $pg )
+    {
+        $pginfo = $pg.GetInfo()
+        $pgvms = $pg.ListProtectedVms().VMName
+        $vmpgcnt = $pgvms.count
+
+        $lo = [pscustomobject] @{
+            ProtectionGroup = $pginfo.Name
+            Description = $pginfo.Description
+            Configured = $pg.CheckConfigured()
+            State = $pg.GetProtectionState().ToString()
+            Type = $pginfo.Type.ToString()
+            Category = $pg.GetType().Name
+            VMCount = $vmpgcnt
+            VMNames = $pgvms
+        }
+        $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.SRM.Protection.Group.Info')
+        return $lo
+    }
+
+    static [pscustomobject] MakeRPInfoObj ( [psobject] $rp )
+    {
+        $arr1 = [System.Collections.Arraylist]::new()
+        $arr2 = [System.Collections.Arraylist]::new()
+        [int] $rpvmcnt = 0
+        $rpinfo = $rp.GetInfo()
+        $prgcnt = $rpinfo.ProtectionGroups.Count
+        foreach ($pg in $rpinfo.ProtectionGroups)
+        {
+            $pgo = [sClass]::MakePGInfoObj($pg)
+            $arr1.add($pgo)
+            $qpg = [sClass]::QueryPGObj($pgo)
+            $rpvmcnt += $qpg.Count
+            if ($qpg.Name)
+            {
+                $arr2.add($qpg.Name)
+            }
+        }
+
+        if ( $arr2 )
+        {
+            $MTpg = $true
+        }
+        elseif ($prgcnt -eq 0)
+        {
+            $MTpg = "NoProtectionGroupsExist"
+        }
+        else
+        {
+            $MTpg = $false
+        }
+
+        $lo = [pscustomobject]@{
+            RecoveryPlan = $rpinfo.Name
+            Description = $rpinfo.Description
+            State = $rpinfo.State.ToString()
+            Type = $rp.GetType().Name.ToString()
+            RecoveryPlanVMCount = $rpvmcnt
+            EmptyProtectionGroup = $MTpg
+            EmptyProtectionGroupName = $arr2
+            ProtectionGroupCount = $prgcnt
+            ProtectionGroups = $arr1
+        }
+        $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.SRM.Recovery.Plan.Info')
+        return $lo
+    }
+
+    static [hashtable] QueryPGObj ( [psobject] $pgo )
+    {
+        $nom = ""
+        $vmc = $pgo.VMCount
+        if ($vmc -eq 0)
+        {
+            $nom = $pgo.ProtectionGroup
+        }
+        $hash = @{
+            Name = $nom
+            Count = $vmc
+        }
+        return $hash
+    }
 }
--- 3.1.2.3/sFunctions.psm1
+++ 3.1.2.3/sFunctions.psm1
@@ -498,6 +498,273 @@ function Show-SRMProtectedVM
 
 <#
 .SYNOPSIS
+Shows Protection Group Detailed Information
+.DESCRIPTION
+Shows Protection Group Detailed Information for submitted Protection Groups.  If no Protection Groups
+or parameters are specified, returns detailed information for all Protection Groups.  See Examples.
+Can be run on recovery or protected site.
+.NOTES
+1. See examples!
+2. Show-SRMProtectionGroupInfo (is equivalent to) Show-SRMProtectionGroupInfo -All
+3. By default a subset of info is displayed. Use Select * , Format-List * , etc. to view all info.
+4. All properties can be output in JSON using the ScriptMethod .Json().  This is for terminal-viewing-convenience.
+.Json() outputs a series of objects, not a list of objects.  For output in a list / to a file, use ConvertTo-Json.
+.PARAMETER All
+Optional.  If specified returns detailed information for all Protection Groups.
+.PARAMETER ProtectionGroup
+Optional.  Protection Group Object.  See Examples.
+[VMware.VimAutomation.Srm.Views.SrmProtectionGroup]
+.INPUTS
+[VMware.VimAutomation.Srm.Views.SrmProtectionGroup]
+.OUTPUTS
+[pscustomobject] SupSkiFun.SRM.Protection.Group.Info
+.EXAMPLE
+See Below:
+Return all SRM Protection Groups into a variable:
+$allPG = Show-SRMProtectionGroupInfo
+
+Default Output:
+$allPG
+
+Extended Output:
+$allPG | Format-List -Property *
+        or
+$allPG | Select-Object -Property *
+.EXAMPLE
+See Below:
+Return specific SRM Protection Group(s) matching a criteria into a variable:
+$myPG = Get-SRMProtectionGroup | Where-Object -Property Name -Match "EX07*"
+$MyVar = $myPG | Show-SRMProtectionGroupInfo
+$MyVar
+.EXAMPLE
+Experimental.  See Notes and Below:
+Output SRM Protection Group Object in JSON:
+$allPG = Show-SRMProtectionGroupInfo
+$allPG.Json()
+.LINK
+Get-SRMProtectionGroup
+Show-SRMRecoveryPlanInfo
+#>
+
+Function Show-SRMProtectionGroupInfo
+{
+    [CmdletBinding(DefaultParameterSetName = "All")]
+
+    Param
+    (
+        [Parameter(ParameterSetName = "All")]
+        [switch]
+        $All,
+
+        [Parameter(ParameterSetName = "PG", ValueFromPipeline = $true)]
+        [VMware.VimAutomation.Srm.Views.SrmProtectionGroup[]]
+        $ProtectionGroup
+    )
+
+    Begin
+    {
+        $srmED =  $DefaultSrmServers.ExtensionData
+
+        if(!$srmED)
+        {
+            Write-Output "Terminating. Session is not connected to a SRM server."
+            break
+        }
+    }
+
+    Process
+    {
+
+        if ($ProtectionGroup)
+        {
+            $pgs = $ProtectionGroup
+        }
+        else
+        {
+            $pgs = $srmED.Protection.ListProtectionGroups()
+        }
+
+        foreach ($pg in $pgs)
+        {
+            $lo = [sClass]::MakePGInfoObj($pg)
+            $lo
+        }
+    }
+
+    End
+    {
+        $TypeName = 'SupSkiFun.SRM.Protection.Group.Info'
+
+        $TypeData1 = @{
+            TypeName = $TypeName
+            MemberType = 'ScriptMethod'
+            MemberName = 'Json'
+            Value = {$this | ConvertTo-Json -Depth 4}
+        }
+
+        $TypeData2 = @{
+            TypeName = $TypeName
+            DefaultDisplayPropertySet = (
+                "ProtectionGroup" ,
+                "Configured" ,
+                "State" ,
+                "VMCount" ,
+                "VMNames"
+            )
+        }
+
+        $TypeData3 = @{
+            TypeName = $TypeName
+            DefaultDisplayProperty = "Name"
+        }
+
+        Update-TypeData @TypeData1 -Force
+        Update-TypeData @TypeData2 -Force
+        Update-TypeData @TypeData3 -Force
+    }
+}
+
+<#
+.SYNOPSIS
+Shows Recovery Plan Detailed Information
+.DESCRIPTION
+Shows Recovery Plan Detailed Information for submitted Recovery Plans including detailed information of
+affiliated Protection Groups.  If no Recovery Plans or parameters are specified, returns detailed information
+for all Recovery Plans.  See Examples.  Can be run on recovery or protected site.
+.NOTES
+1. See examples!
+2. $allRP = Show-SRMRecoveryPlanInfo (is equivalent to) $allRP = Show-SRMRecoveryPlanInfo -All
+3. By default a subset of info is displayed. Use Select * , Format-List * , etc. to view all info.
+4. Access Embedded SRM Protection Groups using property .ProtectionGroups
+5. All properties can be output in JSON using the ScriptMethod .Json().  This is for terminal-viewing-convenience.
+.Json() outputs a series of objects not a list of objects.  For output in a list / to a file, use ConvertTo-Json.
+.PARAMETER All
+Optional.  If specified returns detailed information for all Recovery Plans.
+.PARAMETER RecoveryPlan
+Optional.  SRM Recovery Plan Object.  See Examples.
+[VMware.VimAutomation.Srm.Views.SrmRecoveryPlan].
+.INPUTS
+[VMware.VimAutomation.Srm.Views.SrmRecoveryPlan]
+.OUTPUTS
+[pscustomobject] SupSkiFun.SRM.Recovery.Plan.Info with embedded [pscustomobject] SupSkiFun.SRM.Protection.Group.Info
+.EXAMPLE
+See Below:
+Return all SRM Recovery Plans into a variable:
+$allRP = Show-SRMRecoveryPlanInfo
+
+Default Output:
+$allRP
+
+Extended Output:
+$allRP | Format-List -Property *
+        or
+$allRP | Select-Object -Property *
+.EXAMPLE
+See Below:
+Return specific SRM Recovery Plan(s) matching a criteria into a variable:
+$myRP = Get-SRMRecoveryPlan | Where-Object -Property Name -Match "EX07*"
+$MyVar = $myRP | Show-SRMRecoveryPlanInfo
+
+Default Output:
+$MyVar
+
+Embedded Protection Group Output:
+$MyVar.ProtectionGroups
+.EXAMPLE
+Experimental.  See Notes and Below:
+Output SRM Recovery Plan Object in JSON:
+$allRP = Show-SRMRecoveryPlanInfo
+$allRP.Json()
+.LINK
+Get-SRMRecoveryPlan
+Show-SRMProtectionGroupInfo
+#>
+
+Function Show-SRMRecoveryPlanInfo
+{
+    [CmdletBinding(DefaultParameterSetName = "All")]
+    Param
+    (
+        [Parameter(ParameterSetName = "All")]
+        [switch]
+        $All,
+
+        [Parameter(ParameterSetName = "RP", ValueFromPipeline = $true)]
+        [VMware.VimAutomation.Srm.Views.SrmRecoveryPlan[]]
+        $RecoveryPlan
+    )
+
+    Begin
+    {
+        $srmED =  $DefaultSrmServers.ExtensionData
+
+        if(!$srmED)
+        {
+            Write-Output "Terminating. Session is not connected to a SRM server."
+            break
+        }
+    }
+
+    Process
+    {
+
+        if ($RecoveryPlan)
+        {
+            $rps = $RecoveryPlan
+        }
+        else
+        {
+            $rps = $srmED.Recovery.ListPlans()
+        }
+
+        foreach ($rp in $rps)
+        {
+            $lo = [sClass]::MakeRPInfoObj($rp)
+            $lo
+        }
+    }
+
+    End
+    {
+        $TypeName = 'SupSkiFun.SRM.Recovery.Plan.Info'
+
+        $TypeData1 = @{
+            TypeName = $TypeName
+            MemberType = 'ScriptMethod'
+            MemberName = 'Json'
+            Value = {$this | ConvertTo-Json -Depth 4}
+        }
+
+        $TypeData2 = @{
+            TypeName = $TypeName
+            DefaultDisplayPropertySet = (
+                "RecoveryPlan" ,
+                "State" ,
+                "RecoveryPlanVMCount" ,
+                "EmptyProtectionGroup" ,
+                "EmptyProtectionGroupName" ,
+                "ProtectionGroupCount" ,
+                "ProtectionGroups"
+            )
+        }
+
+        $TypeData3 = @{
+            TypeName = $TypeName
+            DefaultDisplayProperty = "Name"
+        }
+
+        Update-TypeData @TypeData1 -Force
+        Update-TypeData @TypeData2 -Force
+        Update-TypeData @TypeData3 -Force
+    }
+}
+
+
+
+
+
+<#
+.SYNOPSIS
 Shows Relationship amongst Recovery Plans, Protection Groups, and DataStores.
 .DESCRIPTION
 Shows Relationship amongst Recovery Plans, Protection Groups, and DataStores.  Returns an object of RecoveryPlan,
